3b560ed42016e32efc32cfc04ea4bf997906e152
TanerSaydam <tanersaydam@gmail.com>
2024-03-18 16:45:23 +0300

    Add project files.

---
+++ CreditCard2.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.SmartEnum;
+
+namespace ConsoleApp2;
+public sealed class CreditCard2 : SmartEnum<CreditCard2>
+{
+    public static readonly CreditCard2 Standard = new(nameof(Standard), 1);
+    public static readonly CreditCard2 Platinum = new(nameof(Platinum), 2);
+    public static readonly CreditCard2 Premium = new(nameof(Premium), 3);
+    public CreditCard2(string name, int value) : base(name, value)
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp2;
+
+var creditCard = CreditCard.FromValue(2);
+var creditCard2 = CreditCard2.FromName("Standard");
+
+
+Console.WriteLine($"Discount for {creditCard} is {creditCard.Discount:P}");
+
+Console.ReadKey();
